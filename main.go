799ec70a2b8005aeca48c715e32a41deb2f5a5ad
kvnrzl <62183899+kvnrzl@users.noreply.github.com>
2024-09-10 08:04:53 +0700

    add: setup connection to redis

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bitbucket.org/bri_bootcamp/fp-patungan-backend-go/pkg/db"
 	"bitbucket.org/bri_bootcamp/fp-patungan-backend-go/routes"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+
+	db.ConnectToRedis()
 
 	e := echo.New()
 	routes.SetupRoutes(e)
